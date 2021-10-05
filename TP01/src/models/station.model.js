4c0fc0af39f92880a969e1944770d2cba5ccac33
Pierre-Antoine lajoie <lajpie@gmail.com>
2021-10-05 09:38:29 -0400

    station model TP

--- TP01/src/models/station.model.js
+++ TP01/src/models/station.model.js
@@ -1,3 +1,34 @@
 //TODO: modele de la station utilisé pour la création
+import mongoose from "mongoose";
 
+const stationSchema= mongoose.Schema({
+    location:{
+        station: {type:String, unique:true, required:true, uppercase:true},
+        coord:{
+            lon:Number,
+            lat:Number
+        }
+    },
+    temperature:Number,
+    pressure:Number,
+    humidity:Number,
+    feelslike:Number,
+    uvIndex:{type:Number, min:0, max:11},
+    wind:{
+    speed:Number,
+    degree:{type:Number, min:0, max:360}
+    },
+    clouds:{
+        cloudcover:{type:Number, min:0, max:1}
+    },
+    observationDate:{type:Date,  default:Date.now},
+    hexMatrix:[String]
+
+
+}, {
+    collection:'stations',
+    strict:'throw'
+});
+
+export default mongoose.model('Station', stationSchema);
 
