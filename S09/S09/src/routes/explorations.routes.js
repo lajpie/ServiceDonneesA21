83bf82f5c0e97b91a0e6285cb785736475e03a7d
Pierre-Antoine lajoie <lajpie@gmail.com>
2021-10-28 16:59:00 -0400

    fim cours semaine 9

--- S09/S09/src/routes/explorations.routes.js
+++ S09/S09/src/routes/explorations.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import HttpError from 'http-errors';
+import explorationsRepository from '../repositories/explorations.repository.js';
 
 const router = express.Router(); 
 
@@ -14,8 +15,31 @@ class ExplorationsRoutes {
 
     }
 
-    getOne(req, res, next) {
+    async getOne(req, res, next) {
 
+        const retrieveOptions = {};
+        const transformOptions = { embed:{}};
+
+        if(req.query.embed && req.query.embed === 'planet') {
+            retrieveOptions.planet = true;
+            transformOptions.embed.planet = true;
+        }
+
+        try {
+            
+            const idExploration = req.params.explorationId;
+            let exploration = await explorationsRepository.retrieveById(idExploration, retrieveOptions);
+            if (!exploration) {
+                return next(HttpError.NotFound());
+            }
+            exploration = exploration.toObject({getters: false, virtuals:false});
+            exploration = explorationsRepository.transform(exploration, transformOptions);
+
+            res.status(200).json(exploration);
+
+        } catch (err) {
+            return next(err);
+        }
     }
 
 }
