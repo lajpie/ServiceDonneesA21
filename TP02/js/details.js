3fee0e1bf09750767329cb5695bac0738ad403b4
Pierre-Antoine Lajoie <lajpie@gmail.com>
2021-10-29 14:17:29 -0400

    again, up to images

--- TP02/js/details.js
+++ TP02/js/details.js
@@ -1,3 +1,4 @@
+const ELEMENT_IMG_URL = 'https://assets.andromia.science/elements';
 const urlParams = {};
 (window.onpopstate = function () {
     let match;
@@ -17,6 +18,10 @@ console.log(urlParams.monstre)
 $(document).ready(() => {
 
     getMonster(`https://api.andromia.science/monsters/atlas/${urlParams.monstre}`);
+
+    $('#btnGenerate').click(()=>{
+        generateMonster();
+    });
 });
 
 async function getMonster(url){
@@ -33,8 +38,80 @@ async function getMonster(url){
         $('#lblSpeed').html(`[ ${monster.speed.min} - ${monster.speed.max} ]`);
         $('#lblCritical').html(`[ ${(monster.critical.min *100).toFixed(2) } - ${(monster.critical.max *100).toFixed(2) } ]%`);
 
-
-        
-
+        displaySpecimens(monster.specimens);
+      
     };
+}
+
+async function generateMonster(){
+    const GENERATE_URL = `https://api.andromia.science/monsters/${urlParams.monstre}/actions?type=generate`;
+    const response = await axios.post(GENERATE_URL);
+    
+    if(response.status ===201) {
+        const newSpecimen = response.data;
+
+        let specimenHtml = displayNewSpecimen(newSpecimen);
+        $('#infoMonsters tbody').append(specimenHtml);
+    }
+    
+}
+
+function displaySpecimens(specimens){
+    let specimensHtml = ''
+    specimens.forEach(s => {
+      specimensHtml +=  displayNewSpecimen(s);
+    });
+    $('#infoMonsters tbody').html(specimensHtml);
+}
+
+function displayNewSpecimen(s){
+    let specimenHtml = '<tr>';
+
+    specimenHtml += `<td><img src="img/${s.affinity}.png"/></td>`;
+    specimenHtml += `<td>${s.health}</td>`;
+    specimenHtml += `<td>${s.damage}</td>`;
+    specimenHtml += `<td>${s.speed}</td>`;
+    specimenHtml += `<td>${(s.critical *100).toFixed(2)}%</td>`;
+
+    specimenHtml += '<td>';
+    s.talents.forEach(t => {
+        specimenHtml += displayTalent(t);
+    });
+    specimenHtml += `</td>`;
+    
+    specimenHtml += '<td>';
+    s.kernel.forEach(k => {
+        specimenHtml += displayKernel(k);
+    });
+    specimenHtml += `</td>`;
+
+    specimenHtml += `<td>`;
+    specimenHtml += `${s.hash.substring(0,2)}`;
+    specimenHtml += displayHash(s.hash);
+    specimenHtml += `${s.hash.substring(62)}`;
+    specimenHtml += `</td>`;
+
+    specimenHtml += '</tr>';
+
+    return specimenHtml;
+}
+
+function displayTalent(t){
+    let talentHtml = ''
+    talentHtml += `<img src="img/${t}.png"/>`;
+    return talentHtml;
+}
+
+function displayKernel(k){
+    let kernelHtml = '';
+    kernelHtml += `<img class="element" src="${ELEMENT_IMG_URL}/${k}.png" alt="${k}" title="${k}" style="width:50px"/>`;
+    return kernelHtml;
+}
+
+function displayHash(h){
+    let hashColorHtml = ''
+    for (let i = 2; i < 62; i+=6) {
+        hashColorHtml+= `<span class="block" style="color #${ h.substring(i,i+6)}; background-color:#${ h.substring(i,i+6)}; color:#${ h.substring(i,i+6)}" >-</span>`;
+    }
+    return hashColorHtml;
 }
