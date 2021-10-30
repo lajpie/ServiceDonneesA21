5365def7c38b365c9e0b4cfba373f384e0ba4425
Pierre-Antoine Lajoie <lajpie@gmail.com>
2021-10-29 23:11:49 -0400

    images wont load :(

--- TP02/js/details.js
+++ TP02/js/details.js
@@ -18,12 +18,19 @@ $(document).ready(() => {
 
     getMonster(`https://api.andromia.science/monsters/atlas/${urlParams.monstre}`);
 
-    $('#btnGenerate').click(()=>{
+    $('#btnGenerate').click(() => {
         generateMonster();
     });
+
+    $('#btnLocations').click(() => {
+        
+        
+        
+    });
+
 });
 
-async function getMonster(url){
+async function getMonster(url) {
     const response = await axios.get(url);
     if (response.status === 200) {
         const monster = response.data;
@@ -35,49 +42,49 @@ async function getMonster(url){
         $('#lblHealth').html(`[ ${monster.health.min} - ${monster.health.max} ]`);
         $('#lblDamage').html(`[ ${monster.damage.min} - ${monster.damage.max} ]`);
         $('#lblSpeed').html(`[ ${monster.speed.min} - ${monster.speed.max} ]`);
-        $('#lblCritical').html(`[ ${(monster.critical.min *100).toFixed(2) } - ${(monster.critical.max *100).toFixed(2) } ]%`);
+        $('#lblCritical').html(`[ ${(monster.critical.min * 100).toFixed(2)} - ${(monster.critical.max * 100).toFixed(2)} ]%`);
 
         displaySpecimens(monster.specimens);
-      
+        let locationsHtml = displayLocations(monster.locations);
     };
 }
 
-async function generateMonster(){
+async function generateMonster() {
     const GENERATE_URL = `https://api.andromia.science/monsters/${urlParams.monstre}/actions?type=generate`;
     const response = await axios.post(GENERATE_URL);
-    
-    if(response.status ===201) {
+
+    if (response.status === 201) {
         const newSpecimen = response.data;
 
         let specimenHtml = displayNewSpecimen(newSpecimen);
         $('#infoMonsters tbody').append(specimenHtml);
     }
-    
+
 }
 
-function displaySpecimens(specimens){
+function displaySpecimens(specimens) {
     let specimensHtml = ''
     specimens.forEach(s => {
-      specimensHtml +=  displayNewSpecimen(s);
+        specimensHtml += displayNewSpecimen(s);
     });
     $('#infoMonsters tbody').html(specimensHtml);
 }
 
-function displayNewSpecimen(s){
+function displayNewSpecimen(s) {
     let specimenHtml = '<tr>';
 
     specimenHtml += `<td><img src="img/affinities/${s.affinity}.png"/></td>`;
     specimenHtml += `<td>${s.health}</td>`;
     specimenHtml += `<td>${s.damage}</td>`;
     specimenHtml += `<td>${s.speed}</td>`;
-    specimenHtml += `<td>${(s.critical *100).toFixed(2)}%</td>`;
+    specimenHtml += `<td>${(s.critical * 100).toFixed(2)}%</td>`;
 
     specimenHtml += '<td>';
     s.talents.forEach(t => {
         specimenHtml += displayTalent(t);
     });
     specimenHtml += `</td>`;
-    
+
     specimenHtml += '<td>';
     s.kernel.forEach(k => {
         specimenHtml += displayKernel(k);
@@ -85,7 +92,7 @@ function displayNewSpecimen(s){
     specimenHtml += `</td>`;
 
     specimenHtml += `<td>`;
-    specimenHtml += `${s.hash.substring(0,2)}`;
+    specimenHtml += `${s.hash.substring(0, 2)}`;
     specimenHtml += displayHash(s.hash);
     specimenHtml += `${s.hash.substring(62)}`;
     specimenHtml += `</td>`;
@@ -95,22 +102,40 @@ function displayNewSpecimen(s){
     return specimenHtml;
 }
 
-function displayTalent(t){
+function displayTalent(t) {
     let talentHtml = ''
     talentHtml += `<img src="img/affinities/${t}.png"/>`;
     return talentHtml;
 }
 
-function displayKernel(k){
+function displayKernel(k) {
     let kernelHtml = '';
     kernelHtml += `<img class="element" src="img/elements/${k}.png" alt="${k}" title="${k}" style="width:50px"/>`;
     return kernelHtml;
 }
 
-function displayHash(h){
+function displayHash(h) {
     let hashColorHtml = ''
-    for (let i = 2; i < 62; i+=6) {
-        hashColorHtml+= `<span class="block" style="color #${ h.substring(i,i+6)}; background-color:#${ h.substring(i,i+6)}; color:#${ h.substring(i,i+6)}" >-</span>`;
+    for (let i = 2; i < 62; i += 6) {
+        hashColorHtml += `<span class="block" style="color #${h.substring(i, i + 6)}; background-color:#${h.substring(i, i + 6)}; color:#${h.substring(i, i + 6)}" >-</span>`;
     }
     return hashColorHtml;
+}
+
+function displayLocations(locations) {
+    let locationsHtml = ''
+    locations.forEach(l => {
+        locationsHtml += displayNewlocation(l);
+    });
+    $('#locations tbody').html(locationsHtml);
+}
+
+function displayNewlocation(l) {
+    let locationHtml = '<tr>';
+    locationHtml += `<td>${l.position}</td>`;
+    locationHtml += `<td>${l.time}</td>`;
+    locationHtml += `<td><img src="img/seasons/${l.season}"/></td>`;
+    locationHtml += `<td><img src="img/rarities/${l.rates}"/></td>`;
+    locationHtml += '</tr>';
+    return locationHtml;
 }
