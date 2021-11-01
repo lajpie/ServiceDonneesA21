9872a6035b01bcd9027536e1eea1056d71a9c3fc
Pierre-Antoine Lajoie <lajpie@gmail.com>
2021-10-31 21:00:33 -0400

    commentaires et new location

--- TP02/js/details.js
+++ TP02/js/details.js
@@ -22,6 +22,9 @@ $(document).ready(() => {
         generateMonster();
     });
 
+    $('#btnAddLocation').click(()=>{
+        addLocation();
+    });
 
 });
 
@@ -31,6 +34,7 @@ async function getMonster(url) {
         const monster = response.data;
         console.log(monster);
 
+        //on rempli les détails d'un monstre
         $('#id').html(monster.atlasNumber);
         $('#imgIcon').attr('src', monster.assets);
         $('#lblName').html(monster.name);
@@ -39,6 +43,7 @@ async function getMonster(url) {
         $('#lblSpeed').html(`[ ${monster.speed.min} - ${monster.speed.max} ]`);
         $('#lblCritical').html(`[ ${(monster.critical.min * 100).toFixed(2)} - ${(monster.critical.max * 100).toFixed(2)} ]%`);
 
+        //on display les spécimens et les locations déjà dans la base de donnés
         displaySpecimens(monster.specimens);
         displayLocations(monster.locations);
     };
@@ -48,8 +53,12 @@ async function generateMonster() {
     const GENERATE_URL = `https://api.andromia.science/monsters/${urlParams.monstre}/actions?type=generate`;
     const response = await axios.post(GENERATE_URL);
 
+
+    //si la demande faite plus haut fonctionne, on display le nouveau spécimens sous ceux déjà affichés
     if (response.status === 201) {
         const newSpecimen = response.data;
+        
+        console.log(response); // on display dans la console le nouveau spécimen
 
         let specimenHtml = displayNewSpecimen(newSpecimen);
         $('#infoMonsters tbody').append(specimenHtml);
@@ -59,6 +68,7 @@ async function generateMonster() {
 
 function displaySpecimens(specimens) {
     let specimensHtml = ''
+    // pour chaque spécimens dans la listes, on crée de nouveaux spécimens
     specimens.forEach(s => {
         specimensHtml += displayNewSpecimen(s);
     });
@@ -76,19 +86,19 @@ function displayNewSpecimen(s) {
 
     specimenHtml += '<td>';
     s.talents.forEach(t => {
-        specimenHtml += displayTalent(t);
+        specimenHtml += displayTalent(t); //on display chaque talents
     });
     specimenHtml += `</td>`;
 
     specimenHtml += '<td>';
     s.kernel.forEach(k => {
-        specimenHtml += displayKernel(k);
+        specimenHtml += displayKernel(k); //on display chaque kernels
     });
     specimenHtml += `</td>`;
 
     specimenHtml += `<td>`;
     specimenHtml += `${s.hash.substring(0, 2)}`;
-    specimenHtml += displayHash(s.hash);
+    specimenHtml += displayHash(s.hash); //on display les couleurs du hash
     specimenHtml += `${s.hash.substring(62)}`;
     specimenHtml += `</td>`;
 
@@ -111,6 +121,7 @@ function displayKernel(k) {
 
 function displayHash(h) {
     let hashColorHtml = ''
+    //pour les couleurs du hash, on fait une boucle avec des substrings qui incrémentent à coup de 6 (pour faire un hex de couleur)
     for (let i = 2; i < 62; i += 6) {
         hashColorHtml += `<span class="block" style="color #${h.substring(i, i + 6)}; background-color:#${h.substring(i, i + 6)}; color:#${h.substring(i, i + 6)}" >-</span>`;
     }
@@ -122,15 +133,53 @@ function displayLocations(locations) {
     locations.forEach(l => {
         locationsHtml += displayNewlocation(l);
     });
-    $('#locations tbody').html(locationsHtml);
+   
 }
 
-function displayNewlocation(l) {
+function displayNewlocation(l) { //on display les nouvelles locations
     let locationHtml = '<tr>';
     locationHtml += `<td>${l.position}</td>`;
     locationHtml += `<td>${l.time}</td>`;
     locationHtml += `<td><img src="img/seasons/${l.season}.png"/></td>`;
     locationHtml += `<td><img src="img/rarities/${l.rates}.png"/></td>`;
     locationHtml += '</tr>';
-    return locationHtml;
+    $('#locations tbody').append(locationHtml);
 }
+
+//on ajoute une location
+async function addLocation() {
+    //on vérifie si le format fonctionne
+    const isLocationValid = document.getElementById('txtLocation');
+
+    if (isLocationValid) {
+        //si oui, on crée une requête post avec les données de notre forms
+        const position = $('#txtLocation').val();
+        const time = $('#cboTime').val();
+        const season = $('#cboSeason').val();
+        const rates = $('#cboRarity').val();
+
+        const CREATE_LOCATION_URL = `https://api.andromia.science/monsters/atlas/${urlParams.monstre}/locations`;
+
+        const body = {
+            position: position,
+            time: time,
+            season: season,
+            rates: rates
+        }
+
+        const response = await axios.post(CREATE_LOCATION_URL, body);
+
+        if (response.status === 201) { //si la requête fonctionne, on affiche la nouvelle location
+            const newLocation = response.data;
+            const locationlHtml = displayNewlocation(newLocation);
+
+            $('#portals tbody').append(locationlHtml);
+            console.log(response);
+        } else {
+            console.log(response);
+        }
+
+    } else {
+        console.log('Location dans un format invalide.');
+    }
+};
